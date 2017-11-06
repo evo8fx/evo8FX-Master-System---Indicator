8c8f5464e3546f06a20c95de3bae72c75c10e90c
evo8fx <33229248+evo8fx@users.noreply.github.com>
2017-11-06 01:37:43 -0600

    EMA & Scalper Signal
    Added custon Indicator Scalper Signal & standard EMA into code.

--- evo8FX Master System.cs
+++ evo8FX Master System.cs
@@ -18,34 +18,92 @@ namespace cAlgo
         private int totPositions;
         private long totPosVol;
 
-        [Parameter("RiskPercent", DefaultValue = 4.5)]
+        [Parameter("RiskPercent", DefaultValue = 10)]
         public double RiskPercent { get; set; }
 
     /*-- END Summary ---------------------------------------*/
+
 
     /*-- START HeikenAshi Standard ---------------------------------------*/
 
         private IndicatorDataSeries _haOpen;
         private IndicatorDataSeries _haClose;
 
-        [Parameter("Show HeikenAshi", DefaultValue = true)]
+        [Parameter("- Show HeikenAshi ---------", DefaultValue = true)]
         public bool enable_HeikenAshi { get; set; }
 
         [Parameter("Candle width", DefaultValue = 5)]
         public int CandleWidth { get; set; }
 
-        [Parameter("Up color", DefaultValue = "Blue")]
+        [Parameter("Up color", DefaultValue = "Green")]
         public string UpColor { get; set; }
 
         [Parameter("Down color", DefaultValue = "Red")]
         public string DownColor { get; set; }
 
-        private Colors _upColor;
+        private Colors _upColor;        
         private Colors _downColor;
         private bool _incorrectColors;
         private Random _random = new Random();
 
     /*-- START HeikenAshi Standard ---------------------------------------*/
+
+
+    /*-- START Moving Average --------------------------------------------*/
+
+        [Parameter("- Show EMA --------------", DefaultValue = true)]
+        public bool enable_EMA { get; set; }
+        
+        [Parameter("Source")]
+        public DataSeries Source { get; set; }
+
+        [Parameter("Periods", DefaultValue = 14)]
+        public int Periods { get; set; }
+
+        [Output("EMA", Color = Colors.Indigo)]
+        public IndicatorDataSeries Result { get; set; }
+
+        private double exp;
+
+    /*-- END   Moving Average --------------------------------------------*/
+
+
+    /*-- START SCALPER SIGNAL --------------------------------------------*/
+        
+        [Parameter("- Show Scalper Signal -------", DefaultValue = true)]
+        public bool enable_ScalperSignal { get; set; }
+
+        [Parameter("Sensitivity", DefaultValue = 2, MinValue = 1, MaxValue = 3, Step = 1)]
+        public int Sensitivity { get; set; }
+
+        [Parameter("Signal Bar Color", DefaultValue = "Gold")]
+        public string SignalBarColor { get; set; }
+
+        [Output("SS Buy", Color = Colors.LimeGreen, Thickness = 7, PlotType = PlotType.Points)]
+        public IndicatorDataSeries BuyIndicator { get; set; }
+
+        [Output("SS Sell", Color = Colors.Red, Thickness = 7, PlotType = PlotType.Points)]
+        public IndicatorDataSeries SellIndicator { get; set; }
+
+        [Output("SS SignalBarHigh", Color = Colors.Gold, Thickness = 1, PlotType = PlotType.Points)]
+        public IndicatorDataSeries SignalBarHigh { get; set; }
+
+        [Output("SS SignalBarLow", Color = Colors.Gold, Thickness = 1, PlotType = PlotType.Points)]
+        public IndicatorDataSeries SignalBarLow { get; set; }
+
+        enum Signals
+        {
+            None,
+            Buy,
+            Sell
+        }
+
+        private Colors signalBarColor;
+        private Signals lastSignal;
+        private DateTime lastTime;
+        private AverageTrueRange ATR;
+
+    /*-- END SCALPER SIGNAL ----------------------------------------------*/
 
         protected override void Initialize()
         {
@@ -61,6 +119,22 @@ namespace cAlgo
                 if (!Enum.TryParse<Colors>(UpColor, out _upColor) || !Enum.TryParse<Colors>(DownColor, out _downColor))
                     _incorrectColors = true;
             }
+
+            // MOVING AVERAGE -----------------------------------------
+            if (enable_EMA) {
+                exp = 2.0 / (Periods + 1);
+            }
+            
+            // SCALPER SIGNAL -----------------------------------------------
+            if (enable_ScalperSignal) { 
+                if (!Enum.TryParse<Colors>(SignalBarColor, out signalBarColor))
+                    signalBarColor = Colors.Gold;
+
+                lastSignal = Signals.None;
+                lastTime = new DateTime();
+                lastTime = MarketSeries.OpenTime[MarketSeries.Close.Count - 1];
+            }
+
         }
 
         public override void Calculate(int index)
@@ -121,8 +195,119 @@ namespace cAlgo
                 _haOpen[index] = haOpen;
                 _haClose[index] = haClose;
             }
+
+            // MOVING AVERAGE -----------------------------------------
+            if (enable_EMA)
+            {
+                var previousValue = Result[index - 1];
+
+                if (double.IsNaN(previousValue))
+                {
+                    Result[index] = Source[index];
+                }
+                else
+                {
+                    Result[index] = Source[index] * exp + previousValue * (1 - exp);
+                }
+            }
+
+
+            // SCALPER SIGNAL --------------------------------------------
+            if (enable_ScalperSignal)
+            {
+                if (!NewBar(index) || (index < 6))
+                    return;
+
+                ATR = Indicators.AverageTrueRange(14, MovingAverageType.Exponential);
+
+                double bs = BuySignal(index);
+                double ss = SellSignal(index);
+
+                if (bs > 0)
+                {
+                    BuyIndicator[index] = bs;
+                    SignalBarHigh[index - 3] = MarketSeries.High[index - 3];
+                    SignalBarLow[index - 3] = MarketSeries.Low[index - 3];
+                    ChartObjects.DrawLine("SignalBar" + (index - 3), index - 3, SignalBarHigh[index - 3], index - 3, SignalBarLow[index - 3], Colors.Gold, 3, LineStyle.Solid);
+                }
+                else if (ss > 0)
+                {
+                    SellIndicator[index] = ss;
+                    SignalBarHigh[index - 3] = MarketSeries.High[index - 3];
+                    SignalBarLow[index - 3] = MarketSeries.Low[index - 3];
+                    ChartObjects.DrawLine("SignalBar" + (index - 3), index - 3, SignalBarHigh[index - 3], index - 3, SignalBarLow[index - 3], Colors.Gold, 3, LineStyle.Solid);
+
+                }
+            }
+
         }
 
+        /* SCALPER SIGNAL FUNCTION ---------------------------------- */
+        private double SellSignal(int index)
+        {
+            bool ok = true;
+
+            if (Sensitivity > 2)
+                if (MarketSeries.High[index - 6] >= MarketSeries.High[index - 5])
+                    ok = false;
+
+            if (Sensitivity > 1)
+                if (MarketSeries.High[index - 5] >= MarketSeries.High[index - 4])
+                    ok = false;
+
+            if (Sensitivity > 0)
+                if (MarketSeries.High[index - 4] >= MarketSeries.High[index - 3])
+                    ok = false;
+
+            if (ok)
+                if (MarketSeries.Close[index - 2] < MarketSeries.High[index - 3])
+                    if (MarketSeries.Close[index - 1] < MarketSeries.Low[index - 3])
+                    {
+                        lastSignal = Signals.Sell;
+                        return (MarketSeries.High[index] + ATR.Result[index]);
+                    }
+
+            return (double.NaN);
+        }
+
+        private double BuySignal(int index)
+        {
+            bool ok = true;
+
+            if (Sensitivity > 2)
+                if (MarketSeries.Low[index - 6] <= MarketSeries.Low[index - 5])
+                    ok = false;
+
+            if (Sensitivity > 1)
+                if (MarketSeries.Low[index - 5] <= MarketSeries.Low[index - 4])
+                    ok = false;
+
+            if (Sensitivity > 0)
+                if (MarketSeries.Low[index - 4] <= MarketSeries.Low[index - 3])
+                    ok = false;
+
+            if (ok)
+                if (MarketSeries.Close[index - 2] > MarketSeries.Low[index - 3])
+                    if (MarketSeries.Close[index - 1] > MarketSeries.High[index - 3])
+                    {
+                        lastSignal = Signals.Buy;
+                        return (MarketSeries.Low[index] - ATR.Result[index]);
+                    }
+
+            return (double.NaN);
+        }
+
+        private bool NewBar(int index)
+        {
+            if (lastTime != MarketSeries.OpenTime[index])
+            {
+                lastTime = MarketSeries.OpenTime[index];
+                return true;
+            }
+
+            return false;
+        }
+        /* END SCALPER SIGNAL FUNCTION ---------------------------------- */
 
         private void WritetoChart()
         {
